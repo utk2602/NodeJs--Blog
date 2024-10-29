84fb9797eef1c3387abe83bb58c05ffece9fb057
utk2602 <166751207+utk2602@users.noreply.github.com>
2024-10-29 22:31:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const connectDB=require('./server/config/db')
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+connectDB();
+
+app.use(express.static('public'));
+
+
+
+//Templating engines 
+app.use(expressLayout);
+app.set('layout','./layouts/main');
+app.set('view engine','ejs');
+
+app.use('/',require('./server/routes/main'));
+
+app.listen(PORT,()=> {
+    console.log('App listening on port ${PORT}');
+});
